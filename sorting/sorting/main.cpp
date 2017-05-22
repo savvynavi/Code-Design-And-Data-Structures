10a87573e2cea0f9178bc534c3a963181bccb1b9
Georgia Shepherd <fruitbat_08@hotmail.com>
2017-05-22 11:37:26 +0930

    uploading sorting part start

---
+++ sorting/sorting/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bogle(vector<int> &list);
+void bubble(vector<int> &list);
+void insert(vector<int> &list);
+
+int main(){
+
+	system("pause");
+	return 0;
+}
+
+
+//if list isn't sorted, shuffles it and checks again until sorted
+void bogle(vector<int> &list){
+
+}
+
+//compares adjacent entries in list, swaps if out of order, does this until sorted
+void bubble(vector<int> &list){
+
+}
+
+//takes 1 element at a time and moves it backwards through list until sorted
+void insert(vector<int> &list){
+
+}

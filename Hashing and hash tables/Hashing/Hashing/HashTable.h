ac32fd012ae23c0cc90f9893e96b87301e6a5b9d
Georgia Shepherd <fruitbat_08@hotmail.com>
2017-07-25 00:39:16 +0930

    finished hash table, unit tests to be added

--- Hashing and hash tables/Hashing/Hashing/HashTable.h
+++ Hashing and hash tables/Hashing/Hashing/HashTable.h
@@ -6,37 +6,58 @@ using namespace HashFunction;
 template<typename KeyType, typename T>
 class HashTable{
 public:
-	HashTable(unsigned int size) : m_size(size), m_data(new T[size]){}
+	HashTable(unsigned int size): m_size(size), m_data(new T[size]){ 
+		this->clear(); 
+	}
+
 	~HashTable(){ delete[] m_data; }
 
+	//overloaded [] ops
 	T& operator[](const KeyType& key){
-		auto hashedKey = hash(key) % m_size;
+		auto hashedKey = hashFunction(key);
 		return m_data[hashedKey];
 	}
 
 	const T& operator[](const KeyType& key)const{
-		auto hashedKey = hash(key) % m_size;
+		auto hashedKey = hashFunction(key);
 		return m_data[];
 	}
 
-	void print(const KeyType& key){
-		//std::cout << m_data[key];
+	//prints out value at hashed key
+	void print(){
+		for(int i = 0; i < m_size; i++){
+			std::cout << m_data[i] << endl;
+		}
 	}
 
-	//takes in key, hashes using hashFunction, places/removes it in m_data using it's returned index
-	void addValue(const KeyType& key){
-		this->hashFunction(key);
+	//takes in key, hashes using hashFunction, places/removes it in m_data using it's returned index(only if current entry is empty)
+	void addValue(const KeyType& key, T data){
+		int hashKey = this->hashFunction(key);
+		if(m_data[hashKey] == NULL){
+			m_data[hashKey] = data;
+		}else{
+			std::cout << "COLLISION! : " << key << std::endl;
+		}
 	}
-	//void removeValue(const KeyType& key);
 
-	////clears the entire array
-	//void clear();
+	//removes a value at the key by setting it to null
+	void removeValue(const KeyType& key){
+		int hashKey = this->hashFunction(key);
+		m_data[hashKey] = NULL;
 
-	////returns item at given key
-	//unsigned int getValue(const KeyType& key);
+	}
+
+	//clears the entire array by setting all used indexes to null
+	void clear(){ 
+		for(int i = 0; i < m_size; i++){
+			m_data[i] = NULL;
+			//std::cout <<"CLEAR: "<< m_data[i] << endl;
+		}
+	}
 
 private:
-	unsigned int hashFunction(const KeyType& key){	//takes in a key, hashes it and returns index?
+	//takes in a key, hashes it/mods it by array size,returns the index
+	unsigned int hashFunction(const KeyType& key){
 		return(djb2Hash(key) % m_size);
 	}
 
--- Hashing and hash tables/Hashing/Hashing/main.cpp
+++ Hashing and hash tables/Hashing/Hashing/main.cpp
@@ -1,22 +1,43 @@
 #include<iostream>
 #include"HashFunction.h"
 #include"HashTable.h"
+#include<string>
+
 using namespace std;
 using namespace HashFunction;
 
+unsigned char* createChar(std::string key){
+	int size = key.length();
+	unsigned char* tempData = new unsigned char[size];
+	for(int i = 0; i < size; i++){
+		tempData[i] = key[i];
+	}
+	tempData[size] = '\0';
+	return tempData;
+}
+
+
 int main(){
-	unsigned char* data;
-	data = new unsigned char('test');
-	//cout << badHash("test", 4) << endl;
-	//cout << ELFHash(data, 4) << endl;
+	unsigned char* data0 = createChar("test");
+	unsigned char* data1 = createChar("hash browns and errorrrs");
+	unsigned char* data2 = createChar("oh god help me");
+	unsigned char* data3 = createChar("why me");
+	unsigned char* data4 = createChar("worstfunctionever");
+	unsigned char* data5 = createChar("best");
 
-	//template<string, array>
-	HashTable<unsigned char*, int> table(5);
-
-	table.addValue(data);
-	table.print(data);
+	HashTable<unsigned char*, int> table(6);
+	table.print();
 	cout << endl;
-	cout << table[data] << endl;
+	table.addValue(data0, 5);
+	table.addValue(data1, 15);
+	table.addValue(data2, 25);
+	table.addValue(data3, 35);
+	table.addValue(data4, 45);
+	table.addValue(data5, 55);
+
+	table.print();
+
+
 	system("pause");
 	return 0;
 }

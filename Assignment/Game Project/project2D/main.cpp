2a046b31c1c470fb709da7469868637551c07b6d
Georgia Shepherd <fruitbat_08@hotmail.com>
2017-12-04 11:20:03 +1030

    completed class diagram for resource manager, continued creating test cases (initialising causes exception to be thrown, fix this)

--- Assignment/Game Project/project2D/main.cpp
+++ Assignment/Game Project/project2D/main.cpp
@@ -2,17 +2,26 @@
 #define CATCH_CONFIG_RUNNER
 #include"catch.hpp"
 
-//TEST_CASE("testing GSM", "[GameStateManager]"){
-//	GameStateManager testGSM(3);
-//	;
-//	aie::Renderer2D* dRenderer = new aie::Renderer2D;
-//	//MainMenu test(dRenderer, );
-//
-//	//MainMenu test(2dRenderer);
-//}
-//
+TEST_CASE("testing GSM", "[GameStateManager]"){
+	GameStateManager* testGSM = new GameStateManager(2);
+	//aie::Renderer2D* renderer = new aie::Renderer2D();
+
+	//std::shared_ptr<ResourceBase> m_font = ResourceManager::getInstance().get("./font/consolas_italic.ttf", ResourceManager::FONT);
+	//std::shared_ptr<ResourceBase> m_menuTexture = ResourceManager::getInstance().get("./textures/title.png", ResourceManager::TEXTURE);
+	//std::shared_ptr<ResourceBase> m_menuMusic = ResourceManager::getInstance().get("./audio/happy.wav", ResourceManager::AUDIO);
+	
+	//MainMenu* testState = new MainMenu(renderer, m_font, m_menuTexture, m_menuMusic, testGSM);
+	//MainMenu test(dRenderer, );
+
+	//MainMenu test(2dRenderer);
+}
+
 //TEST_CASE("Testing resource manager", "[ResourceManager]"){
-//
+//	std::shared_ptr<ResourceBase> resourceTest;
+//	SECTION("Testing instance count"){
+//		resourceTest = ResourceManager::getInstance().get("./textures/title.png", ResourceManager::TEXTURE);
+//		REQUIRE(resourceTest.ResourceType == 0);
+//	}
 //}
 
 int main(int argc, char* argv[]){
